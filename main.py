023ab2f07b2d96adf0afd9ac27ad84182f4e8b0d
Galor Siboni <galorsiboni@gmail.com>
2021-07-13 05:27:37 +0300

    Length Validation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import can
 from random import randint
 import time
 last_arival = time.time()
+last_dlc_per_id_array = [-1, -1, -1]
 frame_counter = 0
 def generator_unit(frame_counter):
     id_option_array = [0x100, 0x200, 0x300]
@@ -18,18 +19,40 @@ def generator_unit(frame_counter):
 
 def detection_unit(msg):
     invalid_array = [0, 0, 0]
-    global last_arival
+    global last_arival, last_dlc_per_id_array
+
+    # Rate Validation
     rate = time.time() - last_arival
-    if rate <= 0.1:
+    if rate > 0.1:
         invalid_array[0] = 1
     last_arival = time.time()
-    if invalid_array[0] == 1:
-        print("Valid!")
+
+    # Length Validation
+    if msg.arbitration_id == 0x100 and msg.dlc == last_dlc_per_id_array[0]:
+        invalid_array[1] = 1
+    elif msg.arbitration_id == 0x200 and msg.dlc == last_dlc_per_id_array[1]:
+        invalid_array[1] = 1
+    elif msg.arbitration_id == 0x300 and msg.dlc == last_dlc_per_id_array[2]:
+        invalid_array[1] = 1
     else:
-        print("Invalid - rate: {}".format(rate))
+        if msg.arbitration_id == 0x100:
+            last_dlc_per_id_array[0] = msg.dlc
+        elif msg.arbitration_id == 0x200:
+            last_dlc_per_id_array[1] = msg.dlc
+        else:
+            last_dlc_per_id_array[2] = msg.dlc
+    if invalid_array[0] == 0:
+        print("Rate is Valid!")
+    else:
+        print("Rate Invalid - rate: {}".format(rate))
+
+    if invalid_array[1] == 0:
+        print("Length is Valid!")
+    else:
+        print("Length Invalid - length is equal to the previous frame carrying the same ID")
+
     time.sleep(randint(5, 10)/100)
 
 while True:
     detection_unit(generator_unit(frame_counter))
- #   generator_unit(frame_counter)
     frame_counter += 1
