96bb27f51c48613dc5b6b4f8df3609d4da35724b
Galor Siboni <galorsiboni@gmail.com>
2021-07-17 21:06:07 +0300

    fixup

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ CONST_ID_OPTIONS_ARRAY = [0x100, 0x200, 0x300]
 last_arrival = time.time()
 
 # Store the last CAN message DLC for each arbitration id
-last_dlc_per_id_array = [-1] * len(CONST_ID_OPTIONS_ARRAY)
+id_dlc_dict = {}
 
 # Current CAN frame indicator
 frame_counter = 0
+
+# Store the last CAN message data for each arbitration id
+id_data_dict = {}
 
 
 def rate_validation():
@@ -29,44 +32,47 @@ def rate_validation():
 
 
 def length_validation(msg):
-    global last_dlc_per_id_array
+    global id_dlc_dict
+    if id_dlc_dict.get(msg.arbitration_id) is not None:
+        if id_dlc_dict.get(msg.arbitration_id) == msg.dlc:
+            return 1
 
-    # I always save the last message DLC value at a global array call last_dlc_per_id_array that is being initialize
-    # at size of arbitration id options(3 in are case- 0x100, 0x200 and 0x300) with -1 (invalid value)
-    if (msg.arbitration_id == 0x100 and msg.dlc == last_dlc_per_id_array[0]) or (
-            msg.arbitration_id == 0x200 and msg.dlc == last_dlc_per_id_array[1]) or (
-            msg.arbitration_id == 0x300 and msg.dlc == last_dlc_per_id_array[2]):
-        return 1
     else:
-        if msg.arbitration_id == 0x100:
-            last_dlc_per_id_array[0] = msg.dlc
-        elif msg.arbitration_id == 0x200:
-            last_dlc_per_id_array[1] = msg.dlc
-        else:
-            last_dlc_per_id_array[2] = msg.dlc
+        id_dlc_dict[msg.arbitration_id] = msg.dlc
     return 0
 
-# TODO
-def data_validation():
+
+def data_validation(msg):
+    global id_data_dict
+    int_values = [x for x in msg.data]
+
+    if id_data_dict.get(msg.arbitration_id) is not None:
+        for i in range(msg.dlc):
+            if int_values[i] == id_data_dict.get(msg.arbitration_id)[i]:
+                id_data_dict[msg.arbitration_id] = int_values
+                return 1
+
+    else:
+        id_data_dict[msg.arbitration_id] = int_values
     return 0
 
 
 def invalid_reason_checker(invalid_array):
-    invalidReason = "Invalid values: "
+    invalidReason = ""
     if invalid_array[0] == 1 and invalid_array[1] == 0 and invalid_array[2] == 0:
-        return invalidReason + "rate"
+        return invalidReason + "Rate"
     elif invalid_array[0] == 1 and invalid_array[1] == 1 and invalid_array[2] == 0:
-        return invalidReason + "rate and length"
+        return invalidReason + "Rate and Length"
     elif invalid_array[0] == 1 and invalid_array[1] == 0 and invalid_array[2] == 1:
-        return invalidReason + "rate and data"
+        return invalidReason + "Rate and Data"
     elif invalid_array[0] == 1 and invalid_array[1] == 1 and invalid_array[2] == 1:
-        return invalidReason + "rate, length and data"
+        return invalidReason + "Rate, Length and Data"
     elif invalid_array[0] == 0 and invalid_array[1] == 1 and invalid_array[2] == 0:
-        return invalidReason + "length"
+        return invalidReason + "Length"
     elif invalid_array[0] == 0 and invalid_array[1] == 1 and invalid_array[2] == 1:
-        return invalidReason + "length and data"
+        return invalidReason + "length and Data"
     else:
-        return invalidReason + "data"
+        return invalidReason + "Data"
 
 
 def generator_unit():
@@ -74,17 +80,22 @@ def generator_unit():
 
     random_bytearray = []
 
-    # Initialize the data array length between 0-8 bytes and set all values to be equal 1
+    # Initialize the data array length between 0-8 bytes to a random number between 0 - ((2^8) -1)  => Byte size
+    # and fill all the other data field to be constant "1"
     bytearray_size = randint(0, 8)
     for i in range(bytearray_size):
-        random_bytearray.append(1)
+        random_bytearray.append(randint(0, 255))  # 255 = 2^8 - 1
+
+    # fill all the other data field to be constant "1"
+    if bytearray_size != 8:
+        for j in range(bytearray_size, 8):
+            random_bytearray.append(1)
 
     # Create the CAN message
-    can_msg = can.Message(arbitration_id=CONST_ID_OPTIONS_ARRAY[randint(0, 2)], data=random_bytearray,
-                          is_extended_id='False')
-    print("Frame {}: Message id: {},Data: {} , DLC:{}".format(frame_counter, can_msg.arbitration_id, can_msg.data,
-                                                              can_msg.dlc))
-
+    can_msg = can.Message(arbitration_id=CONST_ID_OPTIONS_ARRAY[randint(0, 2)], dlc=bytearray_size,
+                          data=random_bytearray, is_extended_id='False')
+    print("Frame {}: Message id: {},Data: {} , DLC:{}".format(frame_counter, can_msg.arbitration_id,
+                                                              [x for x in can_msg.data], can_msg.dlc))
     return can_msg
 
 
@@ -101,7 +112,7 @@ def detection_unit(msg):
     invalid_array[1] = length_validation(msg)
 
     # Data Validation
-    invalid_array[2] = data_validation()
+    invalid_array[2] = data_validation(msg)
 
     return [msg, invalid_array]
 
@@ -116,11 +127,9 @@ def reporting_unit(results):  # results[0] contain the CAN message & results[1] 
 
     if 1 in results[1]:
         invalidReason = invalid_reason_checker(results[1])
-        logging.error('Frame %s: Timestamp: %s, Frame: %s, Frame Validation: %s, %s', str(frame_counter),
-                      str(can_msg.timestamp), str(hexData), "Invalid", invalidReason)
+        logging.error('Frame %s,%s,%s,%s,%s', frame_counter, str(can_msg.timestamp), str(hexData), "Invalid", invalidReason)
     else:
-        logging.info('Frame %s: Timestamp: %s, Frame: %s, Frame Validation:%s', str(frame_counter),
-                     str(can_msg.timestamp), str(hexData), "Valid")
+        logging.info('%s,%s,%s', str(can_msg.timestamp), str(hexData), "Valid")
 
     # Sleep for random period time of 50mSec to 100 mSec.
     time.sleep(randint(5, 10) / 100)
